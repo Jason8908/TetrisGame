4b1e866da5b316aba95057911063431b7693e9ea
Jason <jasonsu894@gmail.com>
2020-09-25 03:01:07 -0400

    Features and bug fixes.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="javascript/board.js"></script>
 	<script type="text/javascript">
-		window.onload = async () => {
+		function start() {
 			let board = new Board(window.innerWidth, window.innerHeight);
 			board.create();
 			window.onresize = async () => {
@@ -18,15 +18,16 @@
 			let blocks = [L, L1, S, S1, T, I, SQ];
 			//Creating the first block
 			let ran = Math.round(1 + Math.random() * 6);
-			let block = new blocks[ran];
+			//let block = new blocks[ran];
+			let block = new L;
 			block.update();
-			//Main gmae loop
+			//Main game loop
 			setInterval(async () => {
-				if(!block.down()) {
-					let ran = Math.round(1 + Math.random() * 6);
-					block = new blocks[ran];
-					block.update();
-				}
+				// if(!block.down()) {
+				// 	let ran = Math.round(1 + Math.random() * 6);
+				// 	block = new L;
+				// 	block.update();
+				// }
 			}, 400);
 			//Key listeners
 			document.addEventListener('keydown', function(event) {
@@ -37,7 +38,13 @@
 			        block.right();
 			    }
 			    else if(event.key == "ArrowDown") {
-			    	block.down();
+			    	if(!block.down()) {
+			    		block = new L;
+			    		block.update();
+			    	}
+			    }
+			    else if(event.key == "x") {
+			    	block.rRight();
 			    }
 			});
 		};
@@ -57,6 +64,20 @@
 		}, 400);
 		*/
 	</script>
+	<audio id="bgm">
+	  <source src="music/bgm.mp3" type="audio/mpeg">
+	</audio>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$("#play").click(function(event) {
+				/* Act on the event */
+				$("#play").css("display", "none");
+				$('audio#bgm')[0].play();
+				start();
+			});
+		});
+	</script>
 	<body id="body">
+		<img src="images/play.png" id="play">
 	</body>
 </html>
--- javascript/board.js
+++ javascript/board.js
@@ -25,7 +25,7 @@ class Board {
 			for(let x= 0; x < 10; x++) {
 				let box = document.createElement('td');
 				box.id = `${i}-${x}`;
-				box.style.background = '#F1F1F1';
+				box.style.background = '#E1E1E1';
 				//Custom styles.
 				box.style.border = '1px solid white';
 				box.classList.add('off');
@@ -52,6 +52,7 @@ class L{
 	constructor() {
 		this.coords = [[0, 4], [1, 4], [2, 4], [2, 5]];
 		this.active = true;
+		this.rotation = 1;
 	}
 	down() {
 		let y = this.coords.map(y => y[0]);
@@ -106,40 +107,145 @@ class L{
 		for(let x = 0; x < 4; x++) {
 			let sCoords = `${this.coords[x][0]}-${this.coords[x][1]}`;
 			let box = document.getElementById(sCoords);
-			box.style.background = '#F1F1F1';
+			box.style.background = '#E1E1E1';
 			box.classList.add('off');
 			box.classList.remove('on');
 		};
 	}
-	checkY() {
-		let y = this.coords.map(y => y[0]+1);
+	checkY(coords, rotate) {
+		if(!coords) coords = this.coords;
+		let y = coords.map(y => y[0]+1);
 		for(let i = 0; i < y.length; i++) if (y[i] > 19) return false;
-		for(let i = 0; i < this.coords.length; i++) {
-			let box = document.getElementById(`${this.coords[i][0]+1}-${this.coords[i][1]}`);
-			if(box.classList.contains('placed')) return false;
+		for(let i = 0; i < coords.length; i++) {
+			for(let x = 0; x < 2; x++) {
+				let box = document.getElementById(`${coords[i][0]+x}-${coords[i][1]}`);
+				if(box.classList.contains('placed')) return false;
+			};
 		};
 		return true;
 	}
-	checkRight() {
-		let x = this.coords.map(x => x[1]);
-		for(let i = 0; i < x.length; i++) if (x[i] >= 9) return false;
-		for(let i = 0; i < this.coords.length; i++) {
-			let box = document.getElementById(`${this.coords[i][0]}-${this.coords[i][1]+1}`);
-			if(box.classList.contains('placed')) return false;
+	checkRight(coords, rotate) {
+		if(!coords) coords = this.coords;
+		let x = coords.map(x => x[1]);
+		for(let i = 0; i < x.length; i++) if (x[i] >= 9) {
+			if(!rotate) return false;
+			if(x[i] > 9) return false;
+		};
+		for(let i = 0; i < coords.length; i++) {
+			for(let x = 0; x < 2; x++) {
+				let box = document.getElementById(`${coords[i][0]}-${coords[i][1]+x}`);
+				if(box) if(box.classList.contains('placed')) return false;
+			};
 		};
 		return true;
 	}
-	checkLeft() {
-		let x = this.coords.map(x => x[1]);
-		for(let i = 0; i < x.length; i++) if (x[i] <= 0) return false;
-		for(let i = 0; i < this.coords.length; i++) {
-			let box = document.getElementById(`${this.coords[i][0]}-${this.coords[i][1]-1}`);
-			if(box.classList.contains('placed')) return false;
+	checkLeft(coords, rotate) {
+		if(!coords) coords = this.coords;
+		let x = coords.map(x => x[1]);
+		for(let i = 0; i < x.length; i++) if (x[i] <= 0) {
+			if(!rotate) return false;
+			if(x[i] < 0) return false;
+		};
+		for(let i = 0; i < coords.length; i++) {
+			for(let x = 0; x < 2; x++) {
+				let box = document.getElementById(`${coords[i][0]}-${coords[i][1]-x}`);
+				if(box) if(box.classList.contains('placed')) return false;
+			};
 		};
 		return true;
 	}
 	rRight() {
-		
+		switch(this.rotation) {
+			case 1:
+				this.remove();
+				//block movements
+				var temp = this.coords;
+				temp [0][1]+=2;
+				temp [1][0]--;
+				temp [1][1]++;
+				temp [2][0]-=2;
+				temp [3][1]--;
+				temp [3][0]--;
+				//updating blocks
+				if(!this.checkRight(temp, true)) {
+					for(let i = 0; i < 4; i++) temp[i][1]--;
+					if(!this.checkLeft(temp, true)) {
+						for(let i = 0; i < 4; i++) temp[i][0]--;
+						if(this.checkLeft(temp, true) && this.checkRight(temp, true)) this.coords = temp;
+					};
+				};
+				this.update();
+				this.rotation++;
+			break;
+			case 2:
+				this.remove();
+				//block movements
+				var temp = this.coords;
+				temp[0][0]+=2;
+				temp[1][0]++;
+				temp[1][1]++;
+				temp[2][1]+=2;
+				temp[3][0]--;
+				temp[3][1]++;
+				//updating blocks
+				if(!this.checkY(temp, true)) {
+					for(let i = 0; i < 4; i++) temp[i][0]--;
+					if(this.checkLeft(temp, true) && this.checkRight(temp, true) && this.checkY(temp, true)) this.coords = temp;
+				}				
+				this.update();
+				this.rotation++;
+			break;
+			case 3:
+				this.remove();
+				//block movements
+				var temp = [[],[],[],[]];
+				for(let i = 0; i < this.coords.length;i++) {
+					for(let x = 0; x < this.coords[i].length; x++) {
+						temp[i][x] = this.coords[i][x];
+					};
+				};
+				temp [0][1]-=2;
+				temp [1][1]--;
+				temp [1][0]++;
+				temp [2][0]+=2;
+				temp [3][1]++;
+				temp [3][0]++;
+				if(!this.checkLeft(temp, true)) {
+					for(let i = 0; i < 4; i++) temp[i][1]++;
+					if(!this.checkRight(temp, true)) {
+						for(let i = 0; i < 4; i++) temp[i][0]--;
+						if(this.checkLeft(temp, true) && this.checkRight(temp, true)) {
+							this.rotation ++;
+							this.coords = temp;
+						};
+					}
+					else {
+						this.coords = temp;
+						this.rotation ++;
+					};					
+				}
+				else {
+					this.coords = temp;
+					this.rotation ++;
+				};
+				//updating blocks
+				this.update();
+			break;
+			case 4:
+				this.remove();
+				//block movements
+				this.coords[0][0]-=2;
+				this.coords[1][1]--;
+				this.coords[1][0]--;
+				this.coords[2][1]-=2;
+				this.coords[3][1]--;
+				this.coords[3][0]++;
+				//updating blocks
+				this.update();
+				this.rotation = 1;
+			break;
+			
+		};
 	}
 }
 
@@ -201,7 +307,7 @@ class L1{
 		for(let x = 0; x < 4; x++) {
 			let sCoords = `${this.coords[x][0]}-${this.coords[x][1]}`;
 			let box = document.getElementById(sCoords);
-			box.style.background = '#F1F1F1';
+			box.style.background = '#E1E1E1';
 			box.classList.add('off');
 			box.classList.remove('on');
 		};
@@ -293,7 +399,7 @@ class I{
 		for(let x = 0; x < 4; x++) {
 			let sCoords = `${this.coords[x][0]}-${this.coords[x][1]}`;
 			let box = document.getElementById(sCoords);
-			box.style.background = '#F1F1F1';
+			box.style.background = '#E1E1E1';
 			box.classList.add('off');
 			box.classList.remove('on');
 		};
@@ -385,7 +491,7 @@ class SQ{
 		for(let x = 0; x < 4; x++) {
 			let sCoords = `${this.coords[x][0]}-${this.coords[x][1]}`;
 			let box = document.getElementById(sCoords);
-			box.style.background = '#F1F1F1';
+			box.style.background = '#E1E1E1';
 			box.classList.add('off');
 			box.classList.remove('on');
 		};
@@ -477,7 +583,7 @@ class T{
 		for(let x = 0; x < 4; x++) {
 			let sCoords = `${this.coords[x][0]}-${this.coords[x][1]}`;
 			let box = document.getElementById(sCoords);
-			box.style.background = '#F1F1F1';
+			box.style.background = '#E1E1E1';
 			box.classList.add('off');
 			box.classList.remove('on');
 		};
@@ -569,7 +675,7 @@ class S{
 		for(let x = 0; x < 4; x++) {
 			let sCoords = `${this.coords[x][0]}-${this.coords[x][1]}`;
 			let box = document.getElementById(sCoords);
-			box.style.background = '#F1F1F1';
+			box.style.background = '#E1E1E1';
 			box.classList.add('off');
 			box.classList.remove('on');
 		};
@@ -661,7 +767,7 @@ class S1{
 		for(let x = 0; x < 4; x++) {
 			let sCoords = `${this.coords[x][0]}-${this.coords[x][1]}`;
 			let box = document.getElementById(sCoords);
-			box.style.background = '#F1F1F1';
+			box.style.background = '#E1E1E1';
 			box.classList.add('off');
 			box.classList.remove('on');
 		};
